3f1872fa58a7454879469ce42f12a24e71829bfa
Sunbekova <suanbekova05@gmail.com>
2024-10-21 00:07:32 +0500

    Initial Commit

---
+++ Xylophon_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylophon_App
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
