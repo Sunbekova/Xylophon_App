ddf07b68a3777f2cc81f672eaec4a878d8273ac6
Sunbekova <suanbekova05@gmail.com>
2024-10-23 00:14:00 +0500

    completed [without any tap animations]

--- Xylophon_App/ViewController.swift
+++ Xylophon_App/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+        
+    private var audio: AVPlayer?
+    let sounds: [String] = ["A", "B", "C", "D", "E", "F", "G"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func soundPlayerButtons(_ sender: UIButton) {
+        let selectedSound = sounds[sender.tag]
+        playSound(sound: selectedSound)
+    }
+    
+    
+    func playSound(sound: String) {
+        let soundURL = Bundle.main.url(forResource: sound, withExtension: ".wav")
+        audio = AVPlayer(url: soundURL!)
+        audio?.play()
+    }
 }
 
